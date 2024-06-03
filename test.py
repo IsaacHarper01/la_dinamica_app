cd46d84ca678ccb26ef8db4325156f3126de2acb
isaac <isaac.hernandez@optimen.com.mx>
2024-06-03 11:44:55 -0600

    test zbarcam

--- test.py
+++ test.py
@@ -1,29 +1,43 @@
-# from pyzxing import BarCodeReader
-# import os
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.clock import Clock
+# from kivy.uix.label import Label
+# from kivy.uix.button import Button
+# from kivy_garden.zbarcam import ZBarCam
 
-# general_path = os.path.dirname(os.path.abspath(__file__).replace('\\','/'))
-# file = general_path + "/2024-05-28 13.45.46.jpg"
-# print(file)
-# reader = BarCodeReader()
-# decoded_objects = reader.decode(file)
+class QRScanner(BoxLayout):
+    def __init__(self, **kwargs):
+        super(QRScanner, self).__init__(**kwargs)
+        self.orientation = 'vertical'
+        # ZBarCam widget to scan QR codes
+        # self.zbarcam = ZBarCam()
+        # self.add_widget(self.zbarcam)
+        
+        # Label to display scanned QR code
+        # self.qr_label = Label(text="Scanned QR code will appear here")
+        # self.add_widget(self.qr_label)
+        
+        # Button to rescan QR code
+        # self.rescan_button = Button(text="Scan Again")
+        # self.rescan_button.bind(on_press=self.rescan)
+        # self.add_widget(self.rescan_button)
+        
+        # Schedule a method to check for QR codes
+        Clock.schedule_interval(self.check_qr_code, 1)
 
-# if decoded_objects:
-#             qr_code_data = decoded_objects[0].parsed
-#             print(qr_code_data)
+    def check_qr_code(self, dt):
+        if self.zbarcam.symbols:
+            qr_code_data = self.zbarcam.symbols[0].data.decode('utf-8')
+            self.qr_label.text = f"QR code detected: {qr_code_data}"
+            self.zbarcam.play = False  # Stop the camera after detecting a QR code
 
-import sqlite3
-import os
-from datetime import date
+    def rescan(self, instance):
+        self.zbarcam.play = True
+        self.qr_label.text = "Scanned QR code will appear here"
 
-general_path = os.path.dirname(os.path.abspath(__file__).replace('\\','/'))
-present_date = date.today().strftime("%d-%m-%Y")
+class QRScannerApp(App):
+    def build(self):
+        return QRScanner()
 
-conn = sqlite3.connect(f"{general_path}/data/alumnos.db")
-c = conn.cursor()
-query2 = 'SELECT "%s" FROM payments'%present_date
-c.execute(query2)
-data = c.fetchall()
-
-total = sum(num[0] for num in data)
-print(total)
-    
+if __name__ == '__main__':
+    QRScannerApp().run()
